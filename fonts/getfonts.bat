3796d1cead48101ef47505252c7cee03237d79dd
Pete Willard <petewillard@gmail.com>
2018-02-11 09:17:41 -0500

    Initial Upload
    First Unedited Draft of TSRE manual in ASCIIDOCTOR format

---
+++ fonts/getfonts.bat
@@ -0,0 +1,8 @@
+wget http://mirrors.ctan.org/fonts/cm/ps-type1/bakoma/ttf/cmex10.ttf 
+wget http://mirrors.ctan.org/fonts/cm/ps-type1/bakoma/ttf/cmmi10.ttf 
+wget http://mirrors.ctan.org/fonts/cm/ps-type1/bakoma/ttf/cmr10.ttf 
+wget http://mirrors.ctan.org/fonts/cm/ps-type1/bakoma/ttf/cmsy10.ttf 
+wget http://mirrors.ctan.org/fonts/cm/ps-type1/bakoma/ttf/esint10.ttf 
+wget http://mirrors.ctan.org/fonts/cm/ps-type1/bakoma/ttf/eufm10.ttf 
+wget http://mirrors.ctan.org/fonts/cm/ps-type1/bakoma/ttf/msam10.ttf 
+wget http://mirrors.ctan.org/fonts/cm/ps-type1/bakoma/ttf/msbm10.ttf
